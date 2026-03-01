391561b18994f8e69913cef2b67a3e5e1b9746bc
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2026-03-01 14:22:18 -0300

    Finalizado a calculadora com todos os botoes e funcionalidades

--- UAtividade02.dfm
+++ UAtividade02.dfm
@@ -2,8 +2,8 @@ object frmAtividade02: TfrmAtividade02
   Left = 0
   Top = 0
   Caption = 'Atividade 2'
-  ClientHeight = 657
-  ClientWidth = 624
+  ClientHeight = 499
+  ClientWidth = 505
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,100 +12,173 @@ object frmAtividade02: TfrmAtividade02
   Font.Style = []
   TextHeight = 45
   object lbl_resultado: TLabel
-    Left = 528
-    Top = 120
-    Width = 9
+    Left = 344
+    Top = 96
+    Width = 89
     Height = 45
   end
   object lbl_sinal: TLabel
-    Left = 200
-    Top = 120
-    Width = 9
+    Left = 288
+    Top = 96
+    Width = 65
     Height = 45
   end
   object edt_vlr_1: TEdit
-    Left = 24
-    Top = 117
-    Width = 121
+    Left = 25
+    Top = 61
+    Width = 248
     Height = 53
     TabOrder = 0
   end
   object edt_vlr_2: TEdit
-    Left = 288
-    Top = 117
-    Width = 121
+    Left = 25
+    Top = 133
+    Width = 248
     Height = 53
     TabOrder = 1
   end
   object btn_somar: TButton
-    Left = 8
-    Top = 252
-    Width = 129
+    Left = 360
+    Top = 226
+    Width = 137
     Height = 65
     Caption = '+'
     TabOrder = 2
     OnClick = btn_somarClick
   end
   object btn_subtrair: TButton
-    Left = 152
-    Top = 252
-    Width = 129
+    Left = 359
+    Top = 297
+    Width = 138
     Height = 65
     Caption = '-'
     TabOrder = 3
     OnClick = btn_subtrairClick
   end
   object btn_multiplicar: TButton
-    Left = 304
-    Top = 252
-    Width = 129
-    Height = 65
+    Left = 359
+    Top = 368
+    Width = 138
+    Height = 57
     Caption = '*'
     TabOrder = 4
     OnClick = btn_multiplicarClick
   end
   object btn_dividir: TButton
-    Left = 472
-    Top = 252
-    Width = 105
-    Height = 65
+    Left = 360
+    Top = 431
+    Width = 137
+    Height = 57
     Caption = '/'
     TabOrder = 5
     OnClick = btn_dividirClick
   end
   object btn_resultado: TButton
-    Left = 415
-    Top = 118
-    Width = 82
-    Height = 51
+    Left = 248
+    Top = 431
+    Width = 97
+    Height = 57
     Caption = '='
     TabOrder = 6
     OnClick = btn_resultadoClick
   end
   object btn_1: TButton
-    Left = 8
-    Top = 352
+    Left = 25
+    Top = 226
     Width = 97
-    Height = 73
+    Height = 65
     Caption = '1'
     TabOrder = 7
     OnClick = btn_1Click
   end
   object btn_2: TButton
-    Left = 152
-    Top = 352
+    Left = 136
+    Top = 226
     Width = 97
-    Height = 73
+    Height = 65
     Caption = '2'
     TabOrder = 8
     OnClick = btn_2Click
   end
   object btn_3: TButton
-    Left = 280
-    Top = 352
-    Width = 129
-    Height = 73
+    Left = 248
+    Top = 226
+    Width = 97
+    Height = 65
     Caption = '3'
     TabOrder = 9
+    OnClick = btn_3Click
+  end
+  object btn_4: TButton
+    Left = 25
+    Top = 297
+    Width = 97
+    Height = 65
+    Caption = '4'
+    TabOrder = 10
+    OnClick = btn_4Click
+  end
+  object btn_5: TButton
+    Left = 136
+    Top = 297
+    Width = 97
+    Height = 65
+    Caption = '5'
+    TabOrder = 11
+    OnClick = btn_5Click
+  end
+  object btn_6: TButton
+    Left = 248
+    Top = 297
+    Width = 97
+    Height = 65
+    Caption = '6'
+    TabOrder = 12
+    OnClick = btn_6Click
+  end
+  object btn_7: TButton
+    Left = 25
+    Top = 368
+    Width = 97
+    Height = 57
+    Caption = '7'
+    TabOrder = 13
+    OnClick = btn_7Click
+  end
+  object btn_8: TButton
+    Left = 136
+    Top = 368
+    Width = 97
+    Height = 57
+    Caption = '8'
+    TabOrder = 14
+    OnClick = btn_8Click
+  end
+  object btn_9: TButton
+    Left = 248
+    Top = 368
+    Width = 97
+    Height = 57
+    Caption = '9'
+    TabOrder = 15
+    OnClick = btn_9Click
+  end
+  object btn_0: TButton
+    Left = 136
+    Top = 431
+    Width = 97
+    Height = 57
+    Caption = '0'
+    TabOrder = 16
+    OnClick = btn_0Click
+  end
+  object btn_apagar: TButton
+    Left = 25
+    Top = 431
+    Width = 97
+    Height = 57
+    Caption = '<'
+    TabOrder = 17
+    OnClick = btn_apagarClick
   end
 end
--- UAtividade02.pas
+++ UAtividade02.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons;
 
 type
   TfrmAtividade02 = class(TForm)
@@ -20,6 +20,14 @@ type
     btn_1: TButton;
     btn_2: TButton;
     btn_3: TButton;
+    btn_4: TButton;
+    btn_5: TButton;
+    btn_6: TButton;
+    btn_7: TButton;
+    btn_8: TButton;
+    btn_9: TButton;
+    btn_0: TButton;
+    btn_apagar: TButton;
     procedure btn_somarClick(Sender: TObject);
     procedure btn_resultadoClick(Sender: TObject);
     procedure btn_subtrairClick(Sender: TObject);
@@ -27,10 +35,23 @@ type
     procedure btn_dividirClick(Sender: TObject);
     procedure btn_1Click(Sender: TObject);
     procedure btn_2Click(Sender: TObject);
+    procedure btn_3Click(Sender: TObject);
+    procedure btn_4Click(Sender: TObject);
+    procedure btn_5Click(Sender: TObject);
+    procedure btn_6Click(Sender: TObject);
+    procedure btn_7Click(Sender: TObject);
+    procedure btn_8Click(Sender: TObject);
+    procedure btn_9Click(Sender: TObject);
+    procedure btn_0Click(Sender: TObject);
+    procedure btn_apagarClick(Sender: TObject);
+    procedure btn_color_somarClick(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
+    procedure Calcular(const numero1: string; const numero2: string);
+    procedure EnviarNumeroParaLabel(const Valor: string);
+
   end;
 
 var
@@ -40,50 +61,13 @@ implementation
 
 {$R *.dfm}
 
-procedure TfrmAtividade02.btn_1Click(Sender: TObject);
-begin
-  if (lbl_sinal.Caption = '') then
-  begin
-    edt_vlr_1.Text := edt_vlr_1.Text + IntToStr(1);
-  end
-  else
-  begin
-    edt_vlr_2.Text :=  edt_vlr_2.Text + IntToStr(1);
-  end;
-
-
-end;
-
-procedure TfrmAtividade02.btn_2Click(Sender: TObject);
-begin
-  if (lbl_sinal.Caption = '') then
-  begin
-    edt_vlr_1.Text := edt_vlr_1.Text + IntToStr(2);
-  end
-  else
-  begin
-    edt_vlr_2.Text :=  edt_vlr_2.Text + IntToStr(2);
-  end;
-
-end;
-
-procedure TfrmAtividade02.btn_dividirClick(Sender: TObject);
-begin
-    lbl_sinal.Caption := '/';
-end;
-
-procedure TfrmAtividade02.btn_multiplicarClick(Sender: TObject);
-begin
-    lbl_sinal.Caption := '*';
-end;
-
-procedure TfrmAtividade02.btn_resultadoClick(Sender: TObject);
+procedure TfrmAtividade02.Calcular(const numero1: string; const numero2: string);
 var
   n1, n2, resultado : Real;
   sinal: String;
 begin
-  n1 := StrToInt(edt_vlr_1.Text);
-  n2 := StrToInt(edt_vlr_2.Text);
+  n1 := StrToInt(numero1);
+  n2 := StrToInt(numero2);
   sinal := lbl_sinal.Caption;
 
   if (sinal = '+') then
@@ -99,8 +83,97 @@ begin
 
     resultado := n1 / n2;
     lbl_resultado.Caption := FloatToStr(resultado);
+end;
+
+procedure TfrmAtividade02.EnviarNumeroParaLabel(const Valor: string);
+begin
+  if (lbl_sinal.Caption = '') then
+  begin
+    edt_vlr_1.Text := edt_vlr_1.Text + Valor;
+  end
+  else
+  begin
+    edt_vlr_2.Text :=  edt_vlr_2.Text + Valor;
+  end;
+end;
 
 
+procedure TfrmAtividade02.btn_0Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('0') ;
+end;
+
+procedure TfrmAtividade02.btn_1Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('1') ;
+end;
+
+procedure TfrmAtividade02.btn_2Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('2') ;
+end;
+
+procedure TfrmAtividade02.btn_3Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('3') ;
+end;
+
+procedure TfrmAtividade02.btn_4Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('4') ;
+end;
+
+procedure TfrmAtividade02.btn_5Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('5') ;
+end;
+
+procedure TfrmAtividade02.btn_6Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('6') ;
+end;
+
+procedure TfrmAtividade02.btn_7Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('7') ;
+end;
+
+procedure TfrmAtividade02.btn_8Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('8') ;
+end;
+
+procedure TfrmAtividade02.btn_9Click(Sender: TObject);
+begin
+  EnviarNumeroParaLabel('9') ;
+end;
+
+procedure TfrmAtividade02.btn_apagarClick(Sender: TObject);
+begin
+  edt_vlr_1.Text := '';
+  edt_vlr_2.Text := '';
+  lbl_sinal.Caption := '';
+  lbl_resultado.Caption := '';
+end;
+
+procedure TfrmAtividade02.btn_color_somarClick(Sender: TObject);
+begin
+   lbl_sinal.Caption := '+';
+end;
+
+procedure TfrmAtividade02.btn_dividirClick(Sender: TObject);
+begin
+    lbl_sinal.Caption := '/';
+end;
+
+procedure TfrmAtividade02.btn_multiplicarClick(Sender: TObject);
+begin
+    lbl_sinal.Caption := '*';
+end;
+
+procedure TfrmAtividade02.btn_resultadoClick(Sender: TObject);
+begin
+  Calcular(edt_vlr_1.Text,edt_vlr_2.Text)
 end;
 
 procedure TfrmAtividade02.btn_somarClick(Sender: TObject);
