144b68687487311eb0e9056ce2a38b2a528229b3
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2026-04-15 22:36:46 -0300

    .

--- UAtividade33.dfm
+++ UAtividade33.dfm
@@ -2,8 +2,8 @@ object frmAtividade33: TfrmAtividade33
   Left = 0
   Top = 0
   Caption = 'Atividade 33'
-  ClientHeight = 554
-  ClientWidth = 472
+  ClientHeight = 718
+  ClientWidth = 477
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,61 +11,76 @@ object frmAtividade33: TfrmAtividade33
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 45
-  object Shape1: TShape
-    Left = 128
-    Top = 440
-    Width = 225
-    Height = 65
-    Brush.Color = 16744576
-    Pen.Color = clSilver
-  end
-  object lblNome: TLabel
-    Left = 56
-    Top = 29
-    Width = 95
-    Height = 45
-    Caption = 'Nome:'
-  end
-  object lblHora: TLabel
-    Left = 56
-    Top = 160
-    Width = 93
-    Height = 45
-    Caption = 'Hora$:'
-  end
-  object lblHoraTrabalhada: TLabel
-    Left = 57
-    Top = 296
-    Width = 221
-    Height = 45
-    Caption = 'Horas trabalhas'
-  end
-  object btnCalcular: TSpeedButton
-    Left = 128
-    Top = 440
-    Width = 225
-    Height = 65
-    Caption = 'Calcular'
-  end
-  object edtNome: TEdit
-    Left = 56
-    Top = 80
-    Width = 313
-    Height = 53
+  object pnlPrincipal: TPanel
+    Left = 0
+    Top = 0
+    Width = 477
+    Height = 718
+    Align = alClient
+    Color = 15661512
+    ParentBackground = False
     TabOrder = 0
-  end
-  object edtHora: TEdit
-    Left = 56
-    Top = 211
-    Width = 257
-    Height = 53
-    TabOrder = 1
-  end
-  object edtHoraMes: TEdit
-    Left = 56
-    Top = 347
-    Width = 257
-    Height = 53
-    TabOrder = 2
+    ExplicitLeft = 120
+    ExplicitTop = 85
+    ExplicitWidth = 185
+    ExplicitHeight = 41
+    object spCalcular: TShape
+      Left = 128
+      Top = 440
+      Width = 225
+      Height = 65
+      Brush.Color = 16744576
+      Pen.Color = clSilver
+    end
+    object lblNome: TLabel
+      Left = 56
+      Top = 29
+      Width = 95
+      Height = 45
+      Caption = 'Nome:'
+    end
+    object lblHoraTrabalhada: TLabel
+      Left = 57
+      Top = 296
+      Width = 256
+      Height = 45
+      Caption = 'Horas trabalhadas'
+    end
+    object lblHora: TLabel
+      Left = 56
+      Top = 160
+      Width = 93
+      Height = 45
+      Caption = 'Hora$:'
+    end
+    object btnCalcular: TSpeedButton
+      Left = 128
+      Top = 440
+      Width = 225
+      Height = 65
+      Caption = 'Calcular'
+      OnClick = btnCalcularClick
+    end
+    object edtNome: TEdit
+      Left = 56
+      Top = 80
+      Width = 313
+      Height = 53
+      TabOrder = 0
+    end
+    object edtHoraMes: TEdit
+      Left = 56
+      Top = 347
+      Width = 257
+      Height = 53
+      TabOrder = 1
+    end
+    object edtHora: TEdit
+      Left = 56
+      Top = 211
+      Width = 257
+      Height = 53
+      TabOrder = 2
+    end
   end
 end
--- UAtividade33.pas
+++ UAtividade33.pas
@@ -11,12 +11,14 @@ type
     edtNome: TEdit;
     edtHora: TEdit;
     edtHoraMes: TEdit;
-    Shape1: TShape;
+    spCalcular: TShape;
     lblNome: TLabel;
     lblHora: TLabel;
     lblHoraTrabalhada: TLabel;
     btnCalcular: TSpeedButton;
+    pnlPrincipal: TPanel;
     procedure calculoImpostos(salarioBruto: double);
+    procedure btnCalcularClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,7 +26,7 @@ type
     nome: string;
 
     valorHora, horasTrabalhadas, salarioBruto, ir,inss,
-     impostoRenda,salarioLiquido,Sindicato,fgts : double;
+     impostoRenda,salarioLiquido,Sindicato,fgts, totalDescontado : double;
   end;
 
 var
@@ -35,36 +37,79 @@ implementation
 
 procedure TfrmAtividade33.calculoImpostos(salarioBruto: double);
 begin
+  salarioLiquido := 0;
   inss := salarioBruto * 0.10;
   sindicato := salarioBruto * 0.03;
   fgts := salarioBruto * 0.11;
+  nome := edtNome.Text;
 
-  if (salarioBruto > 7500) then
+  ir := 0;
+
+  if salarioBruto > 7500 then
+    ir := salarioBruto * 0.20
+  else if salarioBruto > 3500 then
+    ir := salarioBruto * 0.10
+  else if salarioBruto > 1900 then
+    ir := salarioBruto * 0.05;
+
+  if (ir = 0) then
   begin
-    ir := salarioBruto * 0.20;
-  end
-  else if (salarioBruto > 1900) and (salarioBruto <= 3500) then
-  begin
-      ir := salarioBruto * 0.05;
-  end
-  else if (salarioBruto <= 7500) then
-  begin
-    ir := salarioBruto * 0.10;
+    totalDescontado := inss + sindicato + fgts;
+    salarioLiquido := salarioBruto - inss - sindicato;
+    MessageBox(
+      Application.Handle,
+      PChar(
+        '========== RESUMO DE PAGAMENTO ==========' + #13#10 + #13#10 +
+        'Nome:            ' + nome + #13#10 +
+        '----------------------------------------' + #13#10 +
+        'Salário Bruto:   ' + FormatFloat('R$ ,0.00', salarioBruto) + #13#10 +
+        'INSS:            ' + FormatFloat('R$ ,0.00', inss) + #13#10 +
+        'Sindicato:       ' + FormatFloat('R$ ,0.00', sindicato) + #13#10 +
+        'FGTS:            ' + FormatFloat('R$ ,0.00', fgts) + #13#10 +
+        '----------------------------------------' + #13#10 +
+        'Salário Líquido: ' + FormatFloat('R$ ,0.00', salarioLiquido) + #13#10 +
+        'IR:              ISENTO'
+      ),
+      'Holerite',
+      0
+    );
   end;
 
-
-  if (ir = null) then
+  if (ir <> 0) then
   begin
-    salarioLiquido := salarioBruto - inss - sindicato
-  end;
-
-  if (ir != null) then
-  begin
-     salarioLiquido := salarioBruto - inss - sindicato - ir
+    totalDescontado := inss + sindicato + fgts + ir;
+    salarioLiquido := salarioBruto - inss - sindicato - ir;
+    MessageBox(
+      Application.Handle,
+      PChar(
+        '========== RESUMO ==========' + #13#10 + #13#10 +
+        'Nome:            ' + nome + #13#10 +
+        '--------------------------------' + #13#10 +
+        'Salário Bruto:   ' + FormatFloat('R$ ,0.00', salarioBruto) + #13#10 +
+        'INSS:            ' + FormatFloat('R$ ,0.00', inss) + #13#10 +
+        'Sindicato:       ' + FormatFloat('R$ ,0.00', sindicato) + #13#10 +
+        'FGTS:            ' + FormatFloat('R$ ,0.00', fgts) + #13#10 +
+        '--------------------------------' + #13#10 +
+        'Salário Líquido: ' + FormatFloat('R$ ,0.00', salarioLiquido) + #13#10 +
+        'IR:              ' + FormatFloat('R$ ,0.00', ir)
+      ),
+      'Resumo de Pagamento',
+      0
+    );
   end;
 
 end;
 
 {$R *.dfm}
+
+procedure TfrmAtividade33.btnCalcularClick(Sender: TObject);
+begin
+
+  horasTrabalhadas := StrToFloat(edtHoraMes.Text);
+  valorHora := StrToFloat(edtHora.Text);
+  salarioBruto := horasTrabalhadas * valorHora;
+
+  calculoImpostos(salarioBruto);
+end;
 
 end.
